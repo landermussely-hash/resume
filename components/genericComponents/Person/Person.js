4ae94ff1d9ef52b8d4e2b55ac250032a06f6ac6c
landermussely-hash <landermussely@gmail.com>
2025-11-08 13:00:00 +0100

    render check

--- components/genericComponents/Person/Person.js
+++ components/genericComponents/Person/Person.js
@@ -10,67 +10,67 @@ export default class Person extends Component {
 	render() {
 		const { blok } = this.props;
 
+		// ✅ Debug: check of experiences data binnenkomt
+		console.log("Experiences for", blok.firstname, blok.lastname, ":", blok.experiences);
+
 		return (
-			<>
-				<div {...storyblokEditable(blok)} className={css["wrapper"]}>
-					<div className={css["content"]}>
-						
-						{/* Header */}
-						<div className={[css["box"], css["head"]].join(" ")}>
-							<h1>
-								Resume {blok.title} {blok.lastname} {blok.firstname}
-							</h1>
-						</div>
+			<div {...storyblokEditable(blok)} className={css["wrapper"]}>
+				<div className={css["content"]}>
 
-						{/* Sidebar */}
-						<div className={[css["box"], css["sidebar"]].join(" ")}>
-							<div className={css["personalimage"]}>
-								{blok.image ? (
-									<img
-										src={blok.image}
-										alt={`${blok.firstname} ${blok.lastname}`}
-									/>
-								) : (
-									<p>No image available</p>
-								)}
-							</div>
+					{/* Header */}
+					<div className={[css["box"], css["head"]].join(" ")}>
+						<h1>
+							Resume {blok.title} {blok.lastname} {blok.firstname}
+						</h1>
+					</div>
 
-							<div className={css["personaldetails"]}>
-								<div className={css["personaldetailitem"]}>
-									{blok.title} {blok.firstname} {blok.lastname}
-								</div>
-								<div className={css["personaldetailitem"]}>
-									{blok.dateofbirth}
-								</div>
-								<div className={css["personaldetailitem"]}>
-									{blok.location}
-								</div>
-							</div>
-						</div>
-
-						{/* Experience */}
-						<div className={[css["box"], css["experience"]].join(" ")}>
-							<h2>Experience</h2>
-							{blok.experiences && blok.experiences.length > 0 ? (
-								blok.experiences.map((nestedBlok) => (
-									<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-								))
+					{/* Sidebar */}
+					<div className={[css["box"], css["sidebar"]].join(" ")}>
+						<div className={css["personalimage"]}>
+							{blok.image ? (
+								<img
+									src={blok.image}
+									alt={`${blok.firstname} ${blok.lastname}`}
+								/>
 							) : (
-								<p>No experience added yet.</p>
+								<p>No image available</p>
 							)}
 						</div>
 
-						{/* Footer */}
-						<div className={[css["box"], css["foot"]].join(" ")}>
-							<div>
-								&copy; {blok.firstname} {blok.lastname}{" "}
-								{new Date().getFullYear()}
+						<div className={css["personaldetails"]}>
+							<div className={css["personaldetailitem"]}>
+								{blok.title} {blok.firstname} {blok.lastname}
+							</div>
+							<div className={css["personaldetailitem"]}>
+								{blok.dateofbirth}
+							</div>
+							<div className={css["personaldetailitem"]}>
+								{blok.location}
 							</div>
 						</div>
-
 					</div>
+
+					{/* Experience */}
+					<div className={[css["box"], css["experience"]].join(" ")}>
+						<h2>Experience</h2>
+						{Array.isArray(blok.experiences) && blok.experiences.length > 0 ? (
+							blok.experiences.map((nestedBlok) => (
+								<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+							))
+						) : (
+							<p>No experience added yet.</p>
+						)}
+					</div>
+
+					{/* Footer */}
+					<div className={[css["box"], css["foot"]].join(" ")}>
+						<div>
+							&copy; {blok.firstname} {blok.lastname} {new Date().getFullYear()}
+						</div>
+					</div>
+
 				</div>
-			</>
+			</div>
 		);
 	}
 }
